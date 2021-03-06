995267052d3feab6ea9072b689762cfd6ddb6f7c
Sabina <77441324+rupi412021@users.noreply.github.com>
2021-03-06 20:16:45 +0200

    AjaxCall
    AjaxCalls

--- Digitala/Digitala/Scripts/ajaxCalls.js
+++ Digitala/Digitala/Scripts/ajaxCalls.js
@@ -4,9 +4,6 @@
         url: api,
         data: data,
         cache: false,
-		headers: {
-            'user-key': '79e7344bc82adecdd9d1efdc9b98b543',
-        },
         contentType: "application/json",
         dataType: "json",
         success: successCB,
