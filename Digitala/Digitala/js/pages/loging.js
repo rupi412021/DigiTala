419b67e5b9222b70b33d3cf61c40296e599b7695
Sabina <77441324+rupi412021@users.noreply.github.com>
2021-03-20 20:01:12 +0200

    JS loging page

---
+++ Digitala/Digitala/js/pages/loging.js
@@ -0,0 +1,42 @@
+﻿$(function () {
+    if (localStorage["UserName"] != null) {
+        ajaxCall("GET", "../api/Teachers", "", getUserSuccess, getUserError);
+    }
+
+    else {
+        location.replace("sign-in.html");
+    }
+
+});
+
+function getUserSuccess(data) {
+    user = localStorage["UserName"];
+    isadmin = false;
+    tEmail = "";
+    tname = "";
+
+    for (var i = 0; i < data.length; i++) {
+        if (data[i].TeacherEmail == user) {
+            isadmin = data[i].TeacherAdmin;
+            tEmail = data[i].TeacherEmail;
+            tname = data[i].TeacherFname;
+        }
+
+    }
+
+    str = "<div class='name' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'> שלום " + tname + " </div>" +
+        "<div class='email' >" + tEmail + "</div>";
+    $("#signin").html(str);
+
+    if (isadmin == true) {
+        $("#adminPage").css("display", "block");
+    }
+    else {
+        $("#adminPage").css("display", "none");
+    }
+}
+
+
+function getUserError(error) {
+    console.log(error);
+}
