bfdaf5bd7392af6f7942ff54343fe91f11765841
MorOrovan <77453201+MorOrovan@users.noreply.github.com>
2021-03-30 19:41:44 +0300

    s

--- Digitala/Digitala/js/pages/loging.js
+++ Digitala/Digitala/js/pages/loging.js
@@ -46,16 +46,39 @@ function getStudentError(error) {
     console.log(error);
 }
 
+$(document).on("click", ".openManually", function () {
+    eleId = this.getAttribute('id');
+    arr = document.getElementsByClassName("ml-menu");
+
+    if ($("#" + eleId).hasClass("toggled")) {
+        document.getElementById(eleId).classList.remove("toggled");
+        document.getElementById(eleId).parentNode.classList.remove("active");
+        for (var i = 0; i < arr.length; i++) {
+            if (arr[i].id == eleId) {
+                arr[i].setAttribute("style", "display: none;");
+            }
+        }
+    }
+
+    else {
+        document.getElementById(eleId).classList.add("toggled");
+        document.getElementById(eleId).parentNode.classList.add("active");
+        for (var i = 0; i < arr.length; i++) {
+            if (arr[i].id == eleId)
+                arr[i].setAttribute("style", "display: block;");
+        }
+    }
+});
+
 function getStudentSuccess(Students) {
     str = '';
 
     for (var i = 0; i < Students.length; i++) {
-        str += '<li><a href="javascript:void(0);" class="menu-toggle" ><span>' + Students[i].SFirstName + ' ' + Students[i].SLastName + "</span></a>" +
+        str += '<li><a href="javascript:void(0);" class="menu-toggle waves-effect waves-block openManually" id=' + Students[i].StudentId +'><span>' + Students[i].SFirstName + ' ' + Students[i].SLastName + "</span></a>" +
             '<ul class="ml-menu" id=' + Students[i].StudentId +'><li><a href="StudentProfile.html">תיק אישי</a></li><li><a href="StudentTala.html">תל"א</a></li></ul></li>';
     }
 
     $("#renderStudentsinMenu").html(str);
 }
-
 
  
