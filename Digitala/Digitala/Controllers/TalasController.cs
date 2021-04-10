e201e4ab343ca02700053c0b198ca2e13c182acc
MorOrovan <77453201+MorOrovan@users.noreply.github.com>
2021-04-11 00:03:06 +0300

    x

--- Digitala/Digitala/Controllers/TalasController.cs
+++ Digitala/Digitala/Controllers/TalasController.cs
@@ -17,7 +17,6 @@ namespace Digitala.Controllers
             Talas t = new Talas();
             return t.Read(studentId, year);
         }
-
         [HttpPost]
         [Route("api/Talas")]
         public Talas Post([FromBody]Talas t)
