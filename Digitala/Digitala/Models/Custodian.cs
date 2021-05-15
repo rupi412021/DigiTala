c85a9348f9a211439fc3a39993b21fd76a4f37fc
Sabina <77441324+rupi412021@users.noreply.github.com>
2021-05-15 13:36:16 +0300

    Custodian

---
+++ Digitala/Digitala/Models/Custodian.cs
@@ -0,0 +1,38 @@
+﻿using Digitala.Models.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Digitala.Models
+{
+    public class Custodian
+    {
+        string cEmail;
+        string cName;
+        string cPhone;
+        string cSID;
+
+        public Custodian(string cEmail, string cName, string cPhone, string cSID)
+        {
+            CEmail = cEmail;
+            CName = cName;
+            CPhone = cPhone;
+            CSID = cSID;
+        }
+
+        public Custodian() { }
+        
+        public string CEmail { get => cEmail; set => cEmail = value; }
+        public string CName { get => cName; set => cName = value; }
+        public string CPhone { get => cPhone; set => cPhone = value; }
+        public string CSID { get => cSID; set => cSID = value; }
+
+        public List<Custodian> Insert()
+        {
+            DBServices dbs = new DBServices();
+            dbs.Insert(this);
+            return null; 
+        }
+    }
+}
