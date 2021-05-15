69d1aa2c4506fe2e7ad0ceb3d10745e167e4de1f
MorOrovan <77453201+MorOrovan@users.noreply.github.com>
2021-05-15 18:01:55 +0300

    f

---
+++ Digitala/Digitala/Controllers/TargetsSurveyController.cs
@@ -0,0 +1,43 @@
+﻿using Digitala.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Digitala.Controllers
+{
+    public class TargetsSurveyController : ApiController
+    {
+        [HttpGet]
+        [Route("api/TargetsSurvey")]
+        public List<TargetsSurvey> Get()
+        {
+            TargetsSurvey t = new TargetsSurvey();
+            List<TargetsSurvey> tlist = t.Read();
+            return tlist;
+        }
+
+        [HttpDelete]
+        [Route("api/TargetsSurvey")]
+        public List<TargetsSurvey> Delete([FromBody]TargetsSurvey t)
+        {
+            return t.Delete();
+        }
+
+        [HttpPost]
+        [Route("api/TargetsSurvey")]
+        public List<TargetsSurvey> Post([FromBody]TargetsSurvey t)
+        {
+            return t.Insert();
+        }
+
+        [HttpPut]
+        [Route("api/TargetsSurvey")]
+        public List<TargetsSurvey> Put([FromBody]TargetsSurvey t)
+        {
+            return t.Update();
+        }
+    }
+}
---
+++ Digitala/Digitala/Models/TargetsSurvey.cs
@@ -0,0 +1,88 @@
+﻿using Digitala.Models.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Digitala.Models
+{
+    public class TargetsSurvey
+    {
+        int tarSerial;
+        int tarId;
+        int faSerial;
+        int sfaSerial;
+        string target;
+        string functionArea;
+        string subFunctionArea;
+        double suitability;
+        double originality;
+        int numOfUses;
+        DateTime creationDate;
+        string teacherId;
+
+
+        public TargetsSurvey(int tarSerial, int tarId, int faSerial, int sfaSerial, string target, string functionArea, string subFunctionArea, double suitability, double originality, int numOfUses, DateTime creationDate, string teacherId)
+        {
+            TarSerial = tarSerial;
+            FaSerial = faSerial;
+            SfaSerial = sfaSerial;
+            Target = target;
+            FunctionArea = functionArea;
+            SubFunctionArea = subFunctionArea;
+            Suitability = suitability;
+            Originality = originality;
+            NumOfUses = numOfUses;
+            TarId = tarId;
+            CreationDate = creationDate;
+            TeacherId = teacherId;
+        }
+
+        public TargetsSurvey() { }
+
+        public int TarSerial { get => tarSerial; set => tarSerial = value; }
+        public int TarId { get => tarId; set => tarId = value; }
+        public int FaSerial { get => faSerial; set => faSerial = value; }
+        public int SfaSerial { get => sfaSerial; set => sfaSerial = value; }
+        public string Target { get => target; set => target = value; }
+        public string FunctionArea { get => functionArea; set => functionArea = value; }
+        public string SubFunctionArea { get => subFunctionArea; set => subFunctionArea = value; }
+        public double Suitability { get => suitability; set => suitability = value; }
+        public double Originality { get => originality; set => originality = value; }
+        public int NumOfUses { get => numOfUses; set => numOfUses = value; }
+        public string TeacherId { get => teacherId; set => teacherId = value; }
+        public DateTime CreationDate { get => creationDate; set => creationDate = value; }
+
+
+
+
+        public List<TargetsSurvey> Read()
+        {
+            DBServices dbs = new DBServices();
+            List<TargetsSurvey> tList = dbs.ReadTargetsSurvey();
+            return tList;
+        }
+
+        public List<TargetsSurvey> Delete()
+        {
+            DBServices dbs = new DBServices();
+            dbs.DeleteTarget(this.TarSerial);
+            return Read();
+        }
+
+        public List<TargetsSurvey> Insert()
+        {
+            DBServices dbs = new DBServices();
+            dbs.Insert(this);
+            return Read();
+        }
+
+        public List<TargetsSurvey> Update()
+        {
+            DBServices dbs = new DBServices();
+            dbs.Update(this);
+            return Read();
+        }
+    
+}
+}
